e3b68f6d685ea9404ee4b83171191967e0c8b1fc
tsalesproductions <padilhauch@gmail.com>
2021-08-11 08:03:55 -0300

    Finalizado o relatório padrão dashboard

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -1,9 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { rules, schema } from '@ioc:Adonis/Core/Validator';
 import User from 'App/Models/User';
+import QrData from 'App/Models/QrCode';
 import Database from '@ioc:Adonis/Lucid/Database';
 
 export default class DashboardController {
+  private async searchInDatabase(query, data){
+    // return await QrData.findBy(query, field);
+    return await Database.rawQuery(query, data);
+  }
+
+  private async relatory(){
+    let total_geral = await this.searchInDatabase('SELECT COUNT(id) AS total_geral  FROM qr_codes', []);
+    let total_disponivel = await this.searchInDatabase('SELECT COUNT(id) AS total_geral FROM qr_codes WHERE qr_status = ?', [0]);
+    let total_indisponivel = await this.searchInDatabase('SELECT COUNT(id) AS total_geral FROM qr_codes WHERE qr_status = ?', [1]);
+
+    return {
+      total_geral: total_geral[0][0].total_geral,
+      total_disponivel: total_disponivel[0][0].total_geral,
+      total_indisponivel: total_indisponivel[0][0].total_geral,
+      ganho_estimado: "Breve"
+    }
+  }
+
   public async showIndex({ view, auth, response }: HttpContextContract) {
     await auth.use('web').authenticate();
 
@@ -18,7 +37,8 @@ export default class DashboardController {
 
     return view.render('dashboard/home', {
       user: userData,
-      name
+      name,
+      relatory:  await this.relatory()
     });
   }
 }
